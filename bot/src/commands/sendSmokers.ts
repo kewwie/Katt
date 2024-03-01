b5bcc55e8d620cdad830b5936c60bf4d66c85214
Kewi <iamkewwie@gmail.com>
2024-03-01 11:04:26 +0100

    Update sendSmokers.ts

--- bot/src/commands/sendSmokers.ts
+++ bot/src/commands/sendSmokers.ts
@@ -1,13 +1,20 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
 import { KiwiClient } from "../client";
 
 const command = {
     data: new SlashCommandBuilder()
 		.setName('sendSmokers')
-		.setDescription('Send the list for smokers'),
+		.setDescription('Send the list for smokers')
+		.addStringOption((option: any) =>
+			option.setName('input')
+				.setDescription('The input to echo back')
+				.setDescription('The member to ban')
+				.setRequired(true))
+		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+
 	async execute(interaction: any, client: KiwiClient) {
 
-	// add an option where you make a list with all names you want and add a "," inbetween them to make it work 
+	// add an option where you make a list with all names you want and add a "," inbetween them to make it work
 		await interaction.reply('Made by K3wwie');
 	},
 }
