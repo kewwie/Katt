93f21156ffcdeb5204ca3f8e5d786b960a5e598c
Kewi <iamkewwie@gmail.com>
2024-03-03 00:45:08 +0100

    added stuff for valorant rank

--- bot/src/env.js
+++ bot/src/env.js
@@ -7,9 +7,12 @@ module.exports.env = {
     CLIENT_SECRET: process.env.CLIENT_SECRET,
     CLIENT_TOKEN: process.env.CLIENT_TOKEN,
     SMOKER_LOGS: process.env.SMOKER_LOGS,
+    
     DB_HOST: "mysql",
     DB_PORT: process.env.DATABASE_PORT,
     DB_NAME: "kewwie",
     DB_USER: "root",
     DB_PASSWORD: process.env.DATABASE_ROOT_PASSWORD,
+
+    VALORANT_API_KEY: process.env.VALORANT_API_KEY,
 }
--- bot/src/index.js
+++ bot/src/index.js
@@ -7,5 +7,5 @@ client.login(env.CLIENT_TOKEN);
 
 (async () => {
     await Database.query("CREATE TABLE IF NOT EXISTS nicknames (userId VARCHAR(100) PRIMARY KEY, guildId VARCHAR(100), nickname VARCHAR(100))");
-    await Database.quary("CREATE TABLE IF NOT EXISTS verified (userId VARCHAR(100) PRIMARY KEY, guildId VARCHAR(100), type VARCHAR(25), by VARCHAR(100), autoverify TINYINT(1))");
+    await Database.query("CREATE TABLE IF NOT EXISTS verified (userId VARCHAR(100) PRIMARY KEY, guildId VARCHAR(100), type VARCHAR(25), byUser VARCHAR(100), autoVerify BOOLEAN)");
 })();
