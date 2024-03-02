4831c44c9eed3b0e80b632a9e7e3cb70d5b2ccef
Kewi <iamkewwie@gmail.com>
2024-03-02 22:19:30 +0100

    fixed some issues

--- bot/src/commands/save-nicknames.js
+++ bot/src/commands/save-nicknames.js
@@ -11,9 +11,9 @@ const Database = require("../data/database");
 
 module.exports = {
 	data: new SlashCommandBuilder()
-	.setName('save-nicknames')
-	.setDescription('Saves all nicknames for all users to the database')	
-	.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+		.setName('save-nicknames')
+		.setDescription('Saves all nicknames for all users to the database')	
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
 	/**
     * 
@@ -22,12 +22,14 @@ module.exports = {
     */
 	async execute(interaction, client) {
 		for (var user of interaction.guild.members.cache.values()) {
-			var exist = await Database.query(`SELECT * FROM nicknames WHERE userId = '${user.id}' AND guildId = '${user.guild.id}'`);
-            if (exist[0].length > 0) {
-                await Database.query(`UPDATE nicknames SET nickname = '${user.nickname}' WHERE userId = '${user.id}' AND guildId = '${user.guild.id}'`);
-            } else {
-                await Database.query(`INSERT INTO nicknames (userId, guildId, nickname) VALUES ('${user.id}', '${user.guild.id}', '${user.nickname}')`);
-            }
+			if (user.nickname) {
+				var exist = await Database.query(`SELECT * FROM nicknames WHERE userId = '${user.id}' AND guildId = '${user.guild.id}'`);
+				if (exist[0].length > 0) {
+					await Database.query(`UPDATE nicknames SET nickname = '${user.nickname}' WHERE userId = '${user.id}' AND guildId = '${user.guild.id}'`);
+				} else {
+					await Database.query(`INSERT INTO nicknames (userId, guildId, nickname) VALUES ('${user.id}', '${user.guild.id}', '${user.nickname}')`);
+				}
+			}
 		}
 
 		await interaction.reply({
--- bot/src/events/guildMemberAdd.js
+++ bot/src/events/guildMemberAdd.js
@@ -2,7 +2,7 @@ const { Events, GuildMember } = require("discord.js");
 const Database = require("../data/database");
 
 module.exports = {
-    name: "guildMemberUpdate",
+    name: "guildMemberAdd",
 
     /**
     * 
@@ -10,10 +10,9 @@ module.exports = {
     * @param {GuildMember} member
     */
     async execute(client, member) {
-            var nickname = await Database.query(`SELECT nickname FROM nicknames WHERE userId = '${newMember.id}' AND guildId = '${newMember.guild.id}'`);
-            console.log(nickname);
+            var nickname = await Database.query(`SELECT nickname FROM nicknames WHERE userId = '${member.id}' AND guildId = '${member.guild.id}'`);
             if (nickname[0].length > 0) {
-                member.edit({ nickname: nickname });
+                member.edit({ nickname: nickname[0][0].nickname });
             }
     }
 }
