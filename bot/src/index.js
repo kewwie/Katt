531959ce790d03c230ea801957198df7ea49c4df
Kewi <iamkewwie@gmail.com>
2024-03-04 20:49:56 +0100

    Added verification table

--- bot/src/index.js
+++ bot/src/index.js
@@ -8,4 +8,5 @@ client.login(env.CLIENT_TOKEN);
 (async () => {
     await Database.query("CREATE TABLE IF NOT EXISTS nicknames (userId VARCHAR(100) PRIMARY KEY, guildId VARCHAR(100), nickname VARCHAR(100))");
     await Database.query("CREATE TABLE IF NOT EXISTS verified (userId VARCHAR(100) PRIMARY KEY, guildId VARCHAR(100), type VARCHAR(25), byUser VARCHAR(100), autoVerify BOOLEAN)");
+    await Database.query("CREATE TABLE IF NOT EXISTS verification (guildId VARCHAR(100), role VARCHAR(100), logs VARCHAR(100), pending VARCHAR(100))");
 })();
