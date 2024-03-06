0602ce6791b5ef16cceeb9830848a657f78b296b
Kewi <iamkewwie@gmail.com>
2024-03-06 09:36:27 +0100

    removed console logs

--- bot/src/events/guildMemberAdd.js
+++ bot/src/events/guildMemberAdd.js
@@ -12,7 +12,6 @@ module.exports = {
     async execute(client, member) {
             var nickname = await Database.query(`SELECT nickname FROM nicknames WHERE userId = '${member.id}' AND guildId = '${member.guild.id}'`);
             if (nickname[0].length > 0) {
-                console.log(nickname[0][0].nickname)
                 await member.setNickname(nickname[0][0].nickname);
             }
     }
