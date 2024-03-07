de1aa377a7333f33aee2f8faabe7abcfc807c924
Kewi <iamkewwie@gmail.com>
2024-03-07 16:30:20 +0100

    Update accept-member.js

--- bot/src/buttons/accept-member.js
+++ bot/src/buttons/accept-member.js
@@ -17,18 +17,18 @@ module.exports = {
 
         var servers = await Database.query(`SELECT verificationAdmin, logsChannel, verifiedRole FROM servers WHERE guildId = '${interaction.guildId}'`);
 
-        console.log(servers)
         if (servers[0][0].verificationAdmin && interaction.member.roles.cache.has(servers[0][0].verificationAdmin)) {
 
-            console.log("yes")
+            if (servers[0][0].verifiedRole) {
+                await member.roles.add(servers[0][0].verifiedRole);
+        
+                if (servers[0][0].logsChannel) {
 
-            await member.roles.add(servers[0][0].verificationAdmin);
-            
-
-            if (servers[0][0].logsChannel) {
-
-                var log = await interaction.guild.channels.cache.get(servers[0][0].logsChannel);
-                await log.send(`<@${interaction.user.id}> has moved down **${userToMove}**`);
+                    var log = await interaction.guild.channels.cache.get(servers[0][0].logsChannel);
+                    await log.send({
+                        content: "Verified or smmth ig"
+                    });
+                }
             }
         }
     }
