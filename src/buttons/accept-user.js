be42fb7ac841ee0cfc0770a21a5d4029399413e5
Kewi <iamkewwie@gmail.com>
2024-04-08 12:57:03 +0200

    Updated accept button verification

--- src/buttons/accept-user.js
+++ src/buttons/accept-user.js
@@ -25,53 +25,10 @@ module.exports = {
                 { guildId: interaction.guildId }
             );
             if (guild && type) {
-                
-                if (type === "guest") {
-                    var role = await interaction.guild.roles.cache.find(role => role.id === guild.guestRole);
-                    if (role) {
-                        await member.roles.add(role);
-                    }
-                }
-                if (type === "member") {
-                    var role = await interaction.guild.roles.cache.find(role => role.id === guild.memberRole);
-                    if (role) {
-                        await member.roles.add(role);
-                    }
-                }
-
-                const groups = await client.database.db("kiwi").collection("groups").find({ guildId: interaction.guildId, members: { $in: [memberId] } }).toArray();
-                for (const group of groups) {
-                    const role = await interaction.guild.roles.cache.find(role => role.id === group.roleId);
-                    if (role) {
-                        await member.roles.add(role);
-                    }
-                }
+                client.emit("guildMemberVerify", member, type, interaction.user.id);
 
                 await member.send(`You have been **verified** in **${interaction.guild.name}**`);
                 await interaction.message.delete();
-        
-                if (guild.logsChannel) {
-                    var log = await interaction.guild.channels.cache.get(guild.logsChannel);
-
-                    var addedRoles = member.roles.cache
-                        .filter((roles) => roles.id !== interaction.guildId)
-                        .sort((a, b) => b.rawPosition - a.rawPosition)
-                        .map((role) => role.name);
-
-                    var em = new EmbedBuilder()
-                        .setTitle(member.user.username + "#" + member.user.discriminator)
-                        .setThumbnail(member.user.avatarURL())
-                        .addFields(
-                            { name: "Mention", value: `<@${member.user.id}>` },
-                            { name: "Verified By", value: `<@${interaction.member.id}>` },
-                            { name: "Roles", value: addedRoles.join(", ")}
-                        )
-                        .setColor(0x90EE90)
-
-                    await log.send({
-                        embeds: [em]
-                    });
-                }
             }
         } catch (error) {
             console.log(error);
