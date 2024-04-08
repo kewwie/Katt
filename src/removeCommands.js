f13e97258fa93e3953f17783dfbb4092b47e1f93
Kewi <iamkewwie@gmail.com>
2024-04-08 14:57:03 +0200

    Create removeCommands.js

---
+++ src/removeCommands.js
@@ -0,0 +1,31 @@
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
+const { env } = require("../env");
+
+const guildId = ent.TEST_GUILD; // Guild ID
+
+const rest = new REST({ version: '10' }).setToken(env.CLIENT_TOKEN);
+
+if (guildId) {
+    let data = await rest.get(
+        Routes.applicationGuildCommands(env.CLIENT_ID, guildId)
+    )
+
+    for (let command of data) {
+        await rest.delete(
+            Routes.applicationGuildCommand(env.CLIENT_ID, guildId, command.id)
+        )
+    }
+    console.log(`Successfully unregistered ${data.length} guild (/) commands.`);
+} else {
+    let data = await rest.get(
+        Routes.applicationCommands(env.CLIENT_ID)
+    )
+
+    for (let command of data) {
+        await rest.delete(
+            Routes.applicationCommand(env.CLIENT_ID, command.id)
+        )
+    }
+    console.log(`Successfully unregistered ${data.length} (/) commands.`);
+}
