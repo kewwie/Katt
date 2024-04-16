112533afd220d7151a74ebcbc1c6b8c2ccb01c9c
Kewi <iamkewwie@gmail.com>
2024-04-16 11:51:47 +0200

    Added join thing for nickname

---
+++ app/src/plugins/nicknames/events/guildMemberAdd.ts
@@ -0,0 +1,33 @@
+import {
+	ButtonBuilder,
+	ActionRowBuilder,
+    EmbedBuilder,
+    GuildMember,
+    TextChannel
+} from "discord.js";
+
+import { KiwiClient } from "../../../client";
+
+import { Event, Events } from "../../../types/event";
+
+import { dataSource } from "../../../data/datasource";
+import { Nickname } from "../../../data/entities/Nickname";
+
+export const GuildMemberAdd: Event = {
+    name: Events.GuildMemberAdd,
+
+    /**
+    * 
+    * @param {KiwiClient} client
+    * @param {GuildMember} member
+    */
+    async execute(client: KiwiClient, member: GuildMember) {
+        const NicknameRepository = await dataSource.getRepository(Nickname);
+        var m = await NicknameRepository.findOne(
+            { where: { userId: member.id, guildId: member.guild.id } }
+        );
+        if (m) {
+            member.setNickname(m.nickname);
+        }
+    }
+}
