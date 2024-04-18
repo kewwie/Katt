4fdd58d69e8fe3a1cf2ab164ffa864c397fe2e1e
Kewi <iamkewwie@gmail.com>
2024-04-18 22:36:35 +0200

    Update index.ts

--- app/src/plugins/logs/index.ts
+++ app/src/plugins/logs/index.ts
@@ -1,9 +1,14 @@
 import { Plugin } from "../../types/plugin";
 
+import { GuildMemberUpdate } from "./events/guildMemberUpdate";
+
 export const LogsPlugin: Plugin = {
     config: {
         name: "Logs"
     },
+    events: [
+        GuildMemberUpdate
+    ],
     afterLoad: () => {
         console.log(`Loaded Logs Plugin`)
     }
