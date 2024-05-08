615a049b5995fcece7ab558ce9cfab3eb98cec10
Kewi <iamkewwie@gmail.com>
2024-05-09 01:23:54 +0200

    Removed old commands and events

--- app/src/plugins/verification/index.ts
+++ app/src/plugins/verification/index.ts
@@ -4,11 +4,6 @@ import { ApproveGuest } from "./buttons/approve-guest";
 import { ApproveMember } from "./buttons/approve-member";
 import { DenyUser } from "./buttons/deny-user";
 
-import { BlacklistCmd } from "./commands/blacklist";
-import { WhitelistCmd } from "./commands/whitelist";
-
-import { GuildMemberAdd } from "./events/guildMemberAdd";
-
 export const VerificationPlugin: Plugin = {
     config: {
         name: "Verification",
@@ -18,13 +13,6 @@ export const VerificationPlugin: Plugin = {
         ApproveGuest,
         ApproveMember,
         DenyUser
-    ],
-    commands: [
-        //BlacklistCmd,
-        //WhitelistCmd
-    ],
-    events: [
-        //GuildMemberAdd
     ],
     afterLoad: () => {
         console.log("Loaded Verification Plugin")
