be3c877bfa41d1daeeac2c0a67ba0c03346f7b63
Kewi <iamkewwie@gmail.com>
2024-05-08 23:23:08 +0200

    Updated GuildAdminAdd event

--- app/src/plugins/admin/events/guildAdminAdd.ts
+++ app/src/plugins/admin/events/guildAdminAdd.ts
@@ -8,6 +8,7 @@ import { KiwiClient } from "../../../client";
 import { Event, Events } from "../../../types/event";
 
 import { dataSource } from "../../../data/datasource";
+import { GuildAdmins } from "../../../data/entities/GuildAdmins";
 import { GuildConfig } from "../../../data/entities/GuildConfig";
 
 export const GuildAdminAdd: Event = {
@@ -19,15 +20,19 @@ export const GuildAdminAdd: Event = {
     * @param {User} user
     */
     async execute(client: KiwiClient, guild: Guild, user: User) {
+        const GuildAdminsRepository = await dataSource.getRepository(GuildAdmins);
         const GuildRepository = await dataSource.getRepository(GuildConfig);
         var g = await GuildRepository.findOne({ where: { guildId: guild.id}});
 
-        var guildMember = await guild.members.fetch(user.id);
+        if (await GuildAdminsRepository.findOne({ where: { guildId: guild.id, userId: user.id } })) {
 
-        if (guildMember) {
-            var adminRole = guildMember.roles.cache.find(role => role.id === g.adminRole);
-            if (!adminRole) {
-                await guildMember.roles.add(g.adminRole).catch(() => {});
+            var guildMember = await guild.members.fetch(user.id);
+
+            if (guildMember) {
+                var adminRole = guildMember.roles.cache.find(role => role.id === g.adminRole);
+                if (!adminRole) {
+                    await guildMember.roles.add(g.adminRole).catch(() => {});
+                }
             }
         }
     }
