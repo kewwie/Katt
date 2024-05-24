3f41b0752153273360b6f842f1149af3ad8de360
Kewi <iamkewwie@gmail.com>
2024-05-24 23:01:28 +0200

    Added auto remove member not in group

---
+++ app/src/plugins/groups/events/guildMemberUpdate.ts
@@ -0,0 +1,38 @@
+import {
+    GuildMember
+} from "discord.js";
+
+import { KiwiClient } from "../../../client";
+
+import { Events, Event } from "../../../types/event";
+
+import { dataSource } from "../../../data/datasource";
+import { Group } from "../../../data/entities/Group";
+import { GroupMember } from "../../../data/entities/GroupMember";
+
+/**
+ * @type {Event}
+ */
+export const GuildMemberUpdate: Event = {
+    name: Events.GuildMemberUpdate,
+
+    /**
+    * @param {Client} client
+    * @param {GuildMember} oldMember
+    * @param {GuildMember} newMember
+    */
+    async execute(client: KiwiClient, oldMember: GuildMember, newMember: GuildMember) {
+        const GroupsRepository = await dataSource.getRepository(Group);
+        const GroupMembersRepository = await dataSource.getRepository(GroupMember);
+    
+        newMember.roles.cache.forEach(async (role) => {
+            var group = await GroupsRepository.findOne({ where: { roleId: role.id } });
+            if (group) {
+                var member = await GroupMembersRepository.findOne({ where: { groupId: group.groupId, userId: newMember.id } });
+                if (!member) {
+                    newMember.roles.remove(role.id, "User is not in the group");
+                }
+            }
+        });
+    }
+}
---
+++ app/src/plugins/groups/events/ready.ts
@@ -0,0 +1,61 @@
+import { GuildMember } from "discord.js";
+
+import { KiwiClient } from "../../../client";
+import { Event, Events } from "../../../types/event";
+
+import { dataSource } from "../../../data/datasource";
+import { Group } from "../../../data/entities/Group";
+import { GroupMember } from "../../../data/entities/GroupMember";
+
+import { GroupsPlugin } from "..";
+
+/**
+ * @type {Event}
+ */
+export const Ready: Event = {
+    name: Events.Ready,
+    once: true,
+
+    /**
+    * @param {KiwiClient} client
+    */
+    async execute(client: KiwiClient) {
+        const GroupsRepository = await dataSource.getRepository(Group);
+        const GroupMembersRepository = await dataSource.getRepository(GroupMember);
+
+        for (var guild of await client.guilds.fetch()) {
+            var g = await guild[1].fetch();
+
+            if (await client.getGuildPlugin(g.id, GroupsPlugin.config.name)) {
+                var groups = await GroupsRepository.find({ where: { guildId: g.id }});
+
+                for (let group of groups) {
+                    var members = await GroupMembersRepository.find({ where: { groupId: group.groupId }});
+
+                    for (let member of members) {
+                        var guildMember = await g.members.fetch(member.userId);
+
+                        if (guildMember) {
+                            var role = guildMember.roles.cache.find(role => role.id === group.roleId);
+                            if (!role) {
+                                await guildMember.roles.add(group.roleId).catch(() => {});
+                            }
+                        }
+                    }
+                }
+
+                for (let member of await g.members.fetch()) {
+                    member[1].roles.cache.forEach(async (role) => {
+                        var group = await GroupsRepository.findOne({ where: { roleId: role.id } });
+                        if (group) {
+                            var member = await GroupMembersRepository.findOne({ where: { groupId: group.groupId, userId: member[1].id } });
+                            if (!member) {
+                                member[1].roles.remove(role.id, "User is not in the group");
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    }
+}
