7bc1c6b758848bbeb783a1552db3c3b424ba0e14
Kewi <iamkewwie@gmail.com>
2024-06-05 03:07:02 +0200

    Added event and toggled disableable for levels plugin

--- app/src/plugins/levels/index.ts
+++ app/src/plugins/levels/index.ts
@@ -1,13 +1,19 @@
 import { Plugin } from "../../types/plugin";
 
+
+import { MessageCreate } from "./events/messageCreate";
+
 /**
  * @type {Plugin}
  */
 export const LevelsPlugin: Plugin = {
     config: {
         name: "Levels",
-        disableable: false
+        disableable: true
     },
+    events: [
+        MessageCreate
+    ],
     afterLoad: () => {
         console.log("Loaded Levels Plugin")
     }
