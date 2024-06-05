81b8a73e7206b29f55f61cb3cc43481f69dcbaab
Kewi <iamkewwie@gmail.com>
2024-06-05 19:57:40 +0200

    Moved a bit of code

--- app/src/index.ts
+++ app/src/index.ts
@@ -2,7 +2,7 @@ import { KiwiClient } from "./client";
 import { env } from "./env";
 
 import { dataSource } from "./datasource";
+dataSource.initialize();
 
 const client = new KiwiClient();
-dataSource.initialize();
 client.login(env.CLIENT_TOKEN);
