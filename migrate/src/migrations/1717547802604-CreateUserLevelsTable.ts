79b3037affb83f045ed4304ef50a027524a06d9a
Kewi <iamkewwie@gmail.com>
2024-06-05 02:42:42 +0200

    Added levels table to typeorm

---
+++ migrate/src/migrations/1717547802604-CreateUserLevelsTable.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUserLevelsTable1717547802604 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "user_levels",
+                columns: [
+                    {
+                        name: "_id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment"
+                    },
+                    {
+                        name: "guild_id",
+                        type: "bigint",
+                    },
+                    {
+                        name: "user_id",
+                        type: "bigint",
+                    },
+                    {
+                        name: "level",
+                        type: "int",
+                        unsigned: true,
+                        isNullable: false,
+                        default: 0
+                    },
+                    {
+                        name: "xp",
+                        type: "int",
+                        unsigned: true,
+                        isNullable: false,
+                        default: 0
+                    },
+                    {
+                        name: "last_updated",
+                        type: "datetime",
+                        isNullable: false,
+                        default: "CURRENT_TIMESTAMP"
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("user_levels");
+    }
+
+}
