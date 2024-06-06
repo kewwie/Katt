d4c5ff657b8f6d5a9a7217219d351a53de9a6081
Kewi <iamkewwie@gmail.com>
2024-06-06 23:22:29 +0200

    Fixed so loops gets disabled if plugin is disabled in guild

--- app/src/managers/loop.ts
+++ app/src/managers/loop.ts
@@ -1,4 +1,7 @@
 import { KiwiClient } from "../client";
+import { dataSource } from "../datasource";
+
+import { GuildPlugins } from "../entities/GuildPlugins";
 import { Loop } from "../types/loop";
 
 export class LoopManager {
@@ -8,13 +11,25 @@ export class LoopManager {
         this.client = client;
     }
 
-    load(loops: Loop[]) {
+    async load(loops: Loop[]) {
+        const GuildPluginsRepository = await dataSource.getRepository(GuildPlugins);
+
         for (let loop of loops) {
             this.client.loops.set(loop.name, loop);
 
             setInterval(async () => {
+                var plugin = this.client.PluginManager.plugins.find(plugin => plugin.config.name === loop.plugin);
                 for (let [_, guild] of await this.client.guilds.fetch()) {
-                    await loop.execute(this.client, await guild.fetch());
+                    var g = await guild.fetch();
+
+                    if (!plugin.config.disableable) {
+                        
+                    } else {
+                        var isEnabled = await GuildPluginsRepository.findOne({ where: { guild_id: g.id, plugin: loop.plugin } });
+                        if (isEnabled) {
+                            await loop.execute(this.client, g);
+                        }
+                    }
                 }
             }, loop.seconds * 1000);
         }
