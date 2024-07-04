eb9a11f4ec8d92c472ec1aed0e4fedc073568d52
Kewi <iamkewwie@gmail.com>
2024-07-05 01:04:51 +0200

    fixed null crash

--- app/src/env.ts
+++ app/src/env.ts
@@ -3,15 +3,15 @@ import dotenv from "dotenv";
 dotenv.config({ path: resolve(__dirname, "..", ".env") });
 
 export const env = {
-    CLIENT_ID: process.env.CLIENT_ID as string,
-    CLIENT_TOKEN: process.env.CLIENT_TOKEN as string,
+    CLIENT_ID: process.env.CLIENT_ID as string | null,
+    CLIENT_TOKEN: process.env.CLIENT_TOKEN as string | null,
 
-    STAFF: (process.env.STAFF).split(",") as string[],
-    STAFF_SERVERS: (process.env.STAFF_SERVERS).split(",") as string[],
+    STAFF: (process.env.STAFF?.split(",") || []) as string[],
+    STAFF_SERVERS: (process.env.STAFF_SERVERS?.split(",") || []) as string[],
 
     DB_HOST: "mysql",
-    DB_PORT: process.env.DATABASE_PORT,
+    DB_PORT: process.env.DATABASE_PORT as string | null,
     DB_USER: "iwek",
-    DB_PASSWORD: process.env.DATABASE_PASSWORD,
+    DB_PASSWORD: process.env.DATABASE_PASSWORD as string | null,
     DB_DATABASE: "iwek"
 }
