d5f7beac6da84d61a233974678591e75817296a2
Kewi <iamkewwie@gmail.com>
2024-08-19 11:01:03 +0200

    Updated env for mongodb

--- app/src/env.ts
+++ app/src/env.ts
@@ -3,15 +3,12 @@ import dotenv from "dotenv";
 dotenv.config({ path: resolve(__dirname, "..", ".env") });
 
 export const env = {
-    CLIENT_ID: process.env.CLIENT_ID as string | null,
-    CLIENT_TOKEN: process.env.CLIENT_TOKEN as string | null,
+    CLIENT_ID: process.env.CLIENT_ID as string,
+    CLIENT_TOKEN: process.env.CLIENT_TOKEN as string,
 
     STAFF: (process.env.STAFF?.split(",") || []) as string[],
     STAFF_SERVERS: (process.env.STAFF_SERVERS?.split(",") || []) as string[],
 
-    DB_HOST: "mysql",
-    DB_PORT: process.env.DATABASE_PORT as string | null,
-    DB_USER: "iwek",
-    DB_PASSWORD: process.env.DATABASE_PASSWORD as string | null,
-    DB_DATABASE: "iwek"
+    DATABASE_URL: process.env.DATABASE_URL as string,
+    DATABASE_NAME: process.env.DATABASE_NAME as string,
 }
