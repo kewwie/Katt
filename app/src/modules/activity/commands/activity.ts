4472c4db6ad05e9e102752e5474b54b37f60553a
Kewi <iamkewwie@gmail.com>
2024-10-04 13:41:28 +0200

    added user option

--- app/src/modules/activity/commands/activity.ts
+++ app/src/modules/activity/commands/activity.ts
@@ -18,17 +18,25 @@ import { getPage } from "../utils/getPage";
 export const ActivitySlash: SlashCommand = {
     config: new SlashCommandBuilder()
         .setName("activity")
-        .setDescription("View your's or someone else activity"),
+        .setDescription("View your's or someone else activity")
+        .addUserOption(option =>
+            option
+                .setName("user")
+                .setDescription("The user to view")
+                .setRequired(false)
+        ),
     
     /**
      * @param {ChatInputCommandInteraction} interaction
      * @param {KiwiClient} client
      */
     async execute(interaction: ChatInputCommandInteraction, client: KiwiClient): Promise<void> {
+        var user = interaction.options.getUser("user") || interaction.user;
         var page = await getPage(client, { 
             guildId: interaction.guildId,
             pageId: "status",
-            pageOwner: interaction.user
+            pageOwner: interaction.user,
+            user
         });
         interaction.reply({ embeds: [...page.embeds], components: [...page.rows] });
     },
