a46cbd68bffd6d9449e4a4cde6bc8f331e42dbb8
Kewi <iamkewwie@gmail.com>
2024-10-11 11:31:02 +0200

    added options to leaderboard command

--- app/src/modules/activity/commands/leaderboard.ts
+++ app/src/modules/activity/commands/leaderboard.ts
@@ -14,20 +14,43 @@ import { getLeaderboardPage } from "../utils/getLeaderboardPage";
 export const LeaderboardSlash: SlashCommand = {
     config: new SlashCommandBuilder()
         .setName("leaderboard")
-        .setDescription("View server leaderboards"),
+        .setDescription("View server leaderboards")
+        .addStringOption(option =>
+            option 
+                .setName("type")
+                .setDescription("The leaderboard type")
+                .setRequired(false)
+                .addChoices(
+                    { name: "Voice", value: "voice" },
+                    { name: "Message", value: "message" }
+                )
+        )
+        .addStringOption(option =>
+            option 
+                .setName("time")
+                .setDescription("The leaderboard time")
+                .setRequired(false)
+                .addChoices(
+                    { name: "Total", value: "total" },
+                    { name: "Daily", value: "daily" },
+                    { name: "Weekly", value: "weekly" },
+                    { name: "Monthly", value: "monthly" }
+                )
+        ),
     
     /**
      * @param {ChatInputCommandInteraction} interaction
      * @param {KiwiClient} client
      */
     async execute(interaction: ChatInputCommandInteraction, client: KiwiClient): Promise<void> {
-        var user = interaction.options.getUser("user") || interaction.user;
+        var type = interaction.options.getString("type") || "voice";
+        var time = interaction.options.getString("time") || "total";
+
         var page = await getLeaderboardPage(client, { 
             guildId: interaction.guildId,
-            pageId: "voice",
-            time: "total",
-            pageOwner: interaction.user,
-            user
+            pageId: type,
+            time: time,
+            pageOwner: interaction.user
         });
         interaction.reply({ content: page.content, components: [...page.rows] });
     },
